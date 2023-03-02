c6d621f3226c310343f6de9e6fff4bf25a7600d7
Matvey Pakhomov <matwick12@gmail.com>
2023-03-02 21:17:07 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { arrayWords } from "./constants";
 
 function App() {
+  const shuffleNickname = (enNickname) => {
+    const replaceLetters = (enLetter) => {
+      const filteredArray = arrayWords
+        .map((i) => {
+          if (i[0] === enLetter) {
+            return i[1];
+          } else {
+            return undefined;
+          }
+        })
+        .filter((i) => i !== undefined);
+      if (filteredArray.length > 0) {
+        return filteredArray[0];
+      } else {
+        return enLetter;
+      }
+    };
+    const newNickname = Array.from(enNickname)
+      .map((i) => replaceLetters(i))
+      .join("");
+
+    return (
+      <p className="nickname" id="nickname">
+        {newNickname}
+      </p>
+    );
+  };
+
+  // function highlightRussianLetters(str) {
+  //   const russianLettersRegex = /[а-яё]/gi;
+  //   const russianLetters = str.match(russianLettersRegex);
+  //
+  //   if (!russianLetters) {
+  //     // если нет русских букв, то возвращаем исходную строку
+  //     return str;
+  //   }
+  //
+  //   russianLetters.forEach((letter) => {
+  //     str = str.replace(letter, `<span style="color: red">${letter}</span>`);
+  //   });
+  //
+  //   return str;
+  // }
+  //
+  // function aboba( ) {
+  //   let element = document.querySelector('#nickname');
+  //   element.innerHTML = element.innerHTML.replace('o', '<span style="color: red;">o</span>');
+  // }
+  //
+  // // highlightRussianLetters();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {shuffleNickname("Leorge Gucas")}
       </header>
     </div>
   );
